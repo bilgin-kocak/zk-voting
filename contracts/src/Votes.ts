a34abd2aceb902294137b574d9ecb84fafab40da
Bilgin <kocakbilgin@gmail.com>
2023-11-11 13:31:09 +0300

    feat: votes states added

--- contracts/src/Votes.ts
+++ contracts/src/Votes.ts
@@ -7,16 +7,41 @@ import {
   PublicKey,
   Poseidon,
   MerkleMapWitness,
+  Bool,
+  MerkleMap,
+  MerkleTree,
+  MerkleWitness,
 } from 'o1js';
 
-export class Votes extends SmartContract {
-  //MerkleMap key: PublicKey value: count(Votes)
-  @state(Field) candidates = State<Field>();
-  //Merkle Map: PublicKey    value: 1 (Already has voted)
-  @state(Field) voters = State<Field>();
+const num_voters = 2; // Total Number of Voters
+const options = 2; // TOtal Number of Options
 
-  @method initState(initialRootCandidates: Field, initialRootVoters: Field) {
-    this.candidates.set(initialRootCandidates);
-    this.voters.set(initialRootVoters);
+export class VoterListMerkleWitness extends MerkleWitness(num_voters + 1) {}
+export class VoteCountMerkleWitness extends MerkleWitness(options + 1) {}
+
+export class Votes extends SmartContract {
+  @state(Field) votersMerkleRoot = State<Field>();
+  @state(Field) nullifiersMerkleRoot = State<Field>();
+  @state(Field) voteCountMerkleRoot = State<Field>();
+
+  @state(Bool) isInitialized = State<Bool>();
+  @state(Field) votingID = State<Field>();
+
+  init() {
+    super.init();
+    // Initialize the state variables
+    this.isInitialized.set(Bool(false));
+    this.nullifiersMerkleRoot.set(new MerkleMap().getRoot());
+    this.voteCountMerkleRoot.set(new MerkleTree(options + 1).getRoot());
+    this.votersMerkleRoot.set(Field(0));
+    this.votingID.set(Field(0));
+  }
+
+  @method initState(votingID: Field, votersMerkleTree: Field) {
+    this.isInitialized.assertEquals(Bool(false));
+    // Set vote count root to the root of a new MerkleMap object
+    this.votersMerkleRoot.set(votersMerkleTree);
+    this.votingID.set(votingID);
+    this.isInitialized.set(Bool(true));
   }
 }
